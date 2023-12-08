5a8897f0f8dc6991031ab9f2f53dac0f99b34e4e
Adda Hussain Qazi <addaqazi02@gmail.com>
2023-12-08 17:29:37 +0500

    FORMIK & YUP

--- frontend/src/Pages/register.jsx
+++ frontend/src/Pages/register.jsx
@@ -2,34 +2,96 @@ import React from 'react'
 import Logo from '../assets/logo/crystalvote_icon.png';
 import './auth.css'
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const RegisterSchema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters long')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
+      'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (!@#$%^&*)'
+    ),
+  confirmPassword: Yup.string()
+    .required('Confirm Password is required')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+});
 
 export default function Register() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: (values) => {
+      console.log('Sign Up:', values);
+    },
+  });
+ 
   return (
     <>
- <div className="rect"></div>
-    <div className="main">
-      <div className="container">
-      <div className="image">
-        <img src={Logo} alt="error"/>
-      </div>
-      <div className="inputs">
-        <input type="text" placeholder='Username' />
-      </div>
-      <div className="inputs">
-        <input type="text" placeholder='Password' />
-      </div>
-      <div className="inputs">
-        <input type="text" placeholder='Re-enter Password' />
-      </div>
-      </div>
-      <div className="btn">
-        <input type="button" value="Sign up" />
-      </div>
-      <div className="txt">
-      Already have an account?<Link to="/signin"><span>&nbsp;Sign in</span></Link>
-      </div>
+      <div className="rect"></div>
+      <div className="main">
+        <div className="container">
+          <div className="image">
+            <img src={Logo} alt="error" />
+          </div>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="inputs">
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.username}
+              />
+              {formik.touched.username && formik.errors.username ? (
+                <div className="error">{formik.errors.username}</div>
+              ) : null}
+            </div>
+            <div className="inputs">
+              <input
+                type="text"
+                placeholder="Password"
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <div className="error">{formik.errors.password}</div>
+              ) : null}
+            </div>
+            <div className="inputs">
+              <input
+                type="text"
+                placeholder="Re-enter Password"
+                name="confirmPassword"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.confirmPassword}
+              />
+              {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                <div className="error">{formik.errors.confirmPassword}</div>
+              ) : null}
+            </div>
+            <div className="btn">
+              <input type="submit" value="Sign up" />
+            </div>
+          </form>
+        </div>
+        <div className="txt">
+          Already have an account?
+          <Link to="/signin">
+            <span>&nbsp;Sign in</span>
+          </Link>
+        </div>
       </div>
     </>
-  )
+  );
 }
-
--- frontend/src/Pages/signIn.jsx
+++ frontend/src/Pages/signIn.jsx
@@ -2,32 +2,79 @@ import React from 'react';
 import Logo from '../assets/logo/crystalvote_icon.png';
 import './auth.css'
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
+const SignInSchema = Yup.object().shape({
+  username: Yup.string().required('Username is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters long')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
+      'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (!@#$%^&*)'
+    ),
+});
 
 export default function SignIn() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: SignInSchema,
+    onSubmit: (values) => {
+      console.log('Sign In:', values);
+    },
+  });
+
   return (
     <>
-    <div className="main">
-    <div className="rect"></div>
-      <div className="container">
-      <div className="image">
-        <img src={Logo} alt="error"/>
+      <div className="rect"></div>
+      <div className="main">
+        <div className="container">
+          <div className="image">
+            <img src={Logo} alt="error" />
+          </div>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="inputs">
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.username}
+              />
+              {formik.touched.username && formik.errors.username ? (
+                <div className="error">{formik.errors.username}</div>
+              ) : null}
+            </div>
+            <div className="inputs">
+              <input
+                type="text"
+                placeholder="Password"
+                name="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <div className="error">{formik.errors.password}</div>
+              ) : null}
+            </div>
+            <div className="btn">
+              <input type="submit" value="Sign in" />
+            </div>
+          </form>
+        </div>
+        <div className="txt">
+          Don't have an account?
+          <Link to="/register">
+            <span>&nbsp;Register</span>
+          </Link>
+        </div>
       </div>
-      <div className="inputs">
-        <input type="text" placeholder='Username' />
-      </div>
-      <div className="inputs">
-        <input type="text" placeholder='Password' />
-      </div>
-      </div>
-      <div className="btn">
-        <input type="button" value="Sign in" />
-      </div>
-      <div className="txt">
-        Dont have an account?<Link to="/register"><span>&nbsp;Register</span></Link>
-      </div>
-    </div>
-     
     </>
   )
 }
